37a422bf37414050e0b76d503e07f79f5a24ccc4
Patrick Allen <allenpatrick86@gmail.com>
2016-09-26 16:12:45 -0700

    initial commit for source file

---
+++ chapter-2-exercises.hs
@@ -0,0 +1,6 @@
+---------------- Exercise 2.1 -------------------------
+:type False
+-- False :: Bool
+:type (["foo", "bar"], 'a')
+-- (["foo", "bar"], 'a') :: ([[Char]], Char)
+:type [(True, []), (False, [['a']])]
