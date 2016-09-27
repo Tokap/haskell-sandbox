0e5573410f5bbea7b70b537cd366a4e8eaa94315
Patrick Allen <allenpatrick86@gmail.com>
2016-09-26 17:30:05 -0700

    initial commit

--- chapter-2-exercises.hs
+++ chapter-2-exercises.hs
@@ -7,6 +7,15 @@
 -- [(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
 
 ---------------- Exercise 2.2.1 (Second Excercise Box) -------------------------
+last :: [a] -> a
+-- This function is able to return the final value of any valid list.
+-- The list can be of any type - Boolean, String, Integer, Int, etc.
+-- The list MUST be at least 1 value long, otherwise it will return an exception:
+-- *** Exception: Prelude.last: empty list
+
+-- This function clearly cannot take additional computations, it can't evaluate truthiness,
+-- it can't take in as an argument anything other than a list (a String counts as a list)
+-- it can't talk to outside networks
 
 ---------------- Exercise 2.2.2 (Second Excercise Box) -------------------------
 
