bb008bedd9364cdcd31e62487e02061e1666c9a6
Patrick Allen <allenpatrick86@gmail.com>
2016-09-26 17:09:27 -0700

    add foundation for chapter 2 answers

--- chapter-2-exercises.hs
+++ chapter-2-exercises.hs
@@ -1,6 +1,13 @@
----------------- Exercise 2.1 -------------------------
+---------------- Exercise 2.1.1 (First Excercise Box) -------------------------
 :type False
 -- False :: Bool
 :type (["foo", "bar"], 'a')
 -- (["foo", "bar"], 'a') :: ([[Char]], Char)
 :type [(True, []), (False, [['a']])]
+-- [(True, []), (False, [['a']])] :: [(Bool, [[Char]])]
+
+---------------- Exercise 2.2.1 (Second Excercise Box) -------------------------
+
+---------------- Exercise 2.2.2 (Second Excercise Box) -------------------------
+
+---------------- Exercise 2.2.3 (Second Excercise Box) -------------------------
