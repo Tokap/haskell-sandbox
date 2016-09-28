58f514c2b0760370870b6b7846f6a069e9f181b1
Patrick Allen <allenpatrick86@gmail.com>
2016-09-28 14:39:12 -0700

    initial commit

---
+++ wednesday/chapter-4-exercises.hs
@@ -0,0 +1,17 @@
+---------------- Exercise 4.1.1 (First Excercise Box) -------------------------
+
+-- Do the thing.
+
+---------------- Exercise 4.1.2 (First Excercise Box) -------------------------
+
+-- Do the thing.
+
+---------------- Exercise 4.1.3 (First Excercise Box) -------------------------
+
+-- Do the thing.
+
+---------------- Exercise 4.1.4 (First Excercise Box) -------------------------
+
+-- Do the thing.
+
+---------------- Exercise 4.2.1 (Closing Excercise Box) -------------------------
