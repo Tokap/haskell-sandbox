c56f90895b7e657fdb5791d3823ac2d898c1badb
Patrick Allen <allenpatrick86@gmail.com>
2016-09-28 15:14:35 -0700

    update altLast function

--- wednesday/safe-definitions.hs
+++ wednesday/safe-definitions.hs
@@ -7,8 +7,14 @@ safeTail :: [a] -> Maybe [a]
 safeTail (x:xs) = Just xs
 safeTail _ = Nothing
 
+safeLast :: [a] -> Maybe a
+safeLast (_:xs) = Just (xs !! (length xs - 1))
+safeLast _ = Nothing
 
--- safeLast :: [a] -> Maybe a
---
---
+-- The below function is more efficient as it doesnt call on length
+altLast :: [a] -> Maybe a
+altLast (x:xs) | null xs = Just x
+altLast (x:xs) | not (null xs) = altLast xs
+altLast _ = Nothing
+
 -- safeInit :: [a] -> Maybe [a]
