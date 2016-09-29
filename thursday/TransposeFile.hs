8a65bddc1cda767284d3f0de4925e8ff1aa6176c
Patrick Allen <allenpatrick86@gmail.com>
2016-09-29 11:46:23 -0700

    add notes and demo text file

---
+++ thursday/TransposeFile.hs
@@ -0,0 +1,35 @@
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+        myFunction = id
+
+
+
+
+-- import System.Environment (getArgs)
+--
+-- printList list = mapM_ putStrLn list
+--
+-- -- I feel as if the series of variable assignments below could be condensed
+-- -- into a better function chain. Attempt to refactor after Chapter completion.
+-- readLines fileName = do
+--   parsedFile <- readFile fileName
+--   let fileLines = lines parsedFile
+--   let headOnly = fmap head fileLines
+--   let lineWords = fmap words fileLines --List of Lists broken by line
+--   let firstWords = fmap head lineWords --Just the first word from each line
+--   printList firstWords
+--
+-- main = do
+--   args <- getArgs
+--   case args of
+--     [input] -> readLines input
