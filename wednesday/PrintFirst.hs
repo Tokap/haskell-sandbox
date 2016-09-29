cc0b801151dc77f2b622fa9820640e5bd57cdec3
Patrick Allen <allenpatrick86@gmail.com>
2016-09-29 08:47:20 -0700

    update question solution

--- wednesday/PrintFirst.hs
+++ wednesday/PrintFirst.hs
@@ -2,11 +2,37 @@ import System.Environment (getArgs)
 
 printFirst function inputFile = do
   input <- readFile inputFile
-  mapM_ putStrLn (lines input)
+  printList (lines input)
 
-main = mainWith myFunction
-  where mainWith function = do
-          args <- getArgs
-          case args of
-            [input] -> printFirst function input
-        myFunction = id
+printList list = mapM_ putStrLn list
+
+readLines fileName = do
+  parsedFile <- readFile fileName
+  let fileLines = lines parsedFile
+  let headOnly = fmap head fileLines
+  let lineWords = fmap words fileLines --List of Lists broken by line
+  print lineWords
+
+
+
+-- parseTextLine xs = do
+--   let y = mapM_ head xs
+--   mapM_ putStrLn y
+
+
+-- main = mainWith myFunction
+--   where mainWith function = do
+--           args <- getArgs
+--           case args of
+--             [input] -> printFirst function input
+--         myFunction = id
+
+main = do
+  args <- getArgs
+  case args of
+    [input] -> readLines input
+
+
+
+-- Instead of putStrLn, use a custom function that takes in the line, returns
+-- the first index point printed
