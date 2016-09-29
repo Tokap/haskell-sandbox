c75d29b70580f23e31384edf4cf73042c725da6b
Patrick Allen <allenpatrick86@gmail.com>
2016-09-29 12:43:09 -0700

    add output file

--- thursday/TransposeFile.hs
+++ thursday/TransposeFile.hs
@@ -19,14 +19,20 @@ import System.Environment (getArgs)
 
 printList list = mapM_ putStrLn list
 
+-- joinTuple t =
+
 -- I feel as if the series of variable assignments below could be condensed
 -- into a better function chain. Attempt to refactor after Chapter completion.
-readLines fileName = do
+readLines fileName outFile = do
   parsedFile <- readFile fileName
   let fileLines = lines parsedFile
-  let headOnly = fmap head fileLines
   let lineWords = fmap words fileLines --List of Lists broken by line
-  printList lineWords
+  let brokenDown = concat lineWords -- remove outter list layer
+  let zippedResult = zip (brokenDown !! 0) (brokenDown !! 1) -- zip contents
+  -- take tuples from zipped contents & merge together as string and add newline.
+  let testResult = concatMap (\(a,b) -> [a,b] ++ "\n") zippedResult
+  writeFile outFile testResult
+  -- print ( testResult )
 
 main = do
   args <- getArgs
