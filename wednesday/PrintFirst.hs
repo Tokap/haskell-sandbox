7d3ea759c74b8ffa16622283fe574d1f61af325d
Patrick Allen <allenpatrick86@gmail.com>
2016-09-29 09:06:43 -0700

    complete question pertaining to printing first word from each line

--- wednesday/PrintFirst.hs
+++ wednesday/PrintFirst.hs
@@ -1,9 +1,5 @@
 import System.Environment (getArgs)
 
-printFirst function inputFile = do
-  input <- readFile inputFile
-  printList (lines input)
-
 printList list = mapM_ putStrLn list
 
 readLines fileName = do
@@ -11,28 +7,10 @@ readLines fileName = do
   let fileLines = lines parsedFile
   let headOnly = fmap head fileLines
   let lineWords = fmap words fileLines --List of Lists broken by line
-  print lineWords
-
-
-
--- parseTextLine xs = do
---   let y = mapM_ head xs
---   mapM_ putStrLn y
-
-
--- main = mainWith myFunction
---   where mainWith function = do
---           args <- getArgs
---           case args of
---             [input] -> printFirst function input
---         myFunction = id
+  let firstWords = fmap head lineWords --Just the first word from each line
+  printList firstWords
 
 main = do
   args <- getArgs
   case args of
     [input] -> readLines input
-
-
-
--- Instead of putStrLn, use a custom function that takes in the line, returns
--- the first index point printed
