552750d62ad30d9f4bdebcb2c2c1b5e5e98510e5
Patrick Allen <allenpatrick86@gmail.com>
2016-09-30 12:05:22 -0700

    add notes on functions

--- friday/LYAHFGG-Notes.hs
+++ friday/LYAHFGG-Notes.hs
@@ -30,3 +30,56 @@ quicksort (x:xs) =
 -- return values. A function that does either of those is called a higher order
 -- function. Higher order functions aren't just a part of the Haskell experience,
 -- they pretty much are the Haskell experience.
+
+----- Curried functions
+
+-- Functions that take more than one parameter actually take in 1 param and
+-- return a function with that param that takes another param and, say, compares
+-- The below statements are equivalent:
+ghci> max 4 5
+5
+ghci> (max 4) 5
+5
+
+----- Partial Functions (Below is equivalent)
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+-- The type declaration stays the same, because compare 100 returns a function.
+-- Compare has a type of (Ord a) => a -> (a -> Ordering) and calling it with 100
+-- returns a (Num a, Ord a) => a -> Ordering. The additional class constraint
+-- sneaks up there because 100 is also part of the Num typeclass.
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+-- Taking functions & returning them as params & output
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+-- Before, we didn't need parentheses because -> is naturally right-associative.
+-- However, here, they're mandatory. They indicate that the first parameter is a
+-- function that takes something and returns that same thing.
+
+---------- zipWith Example and Explanation
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+-- Look at the type declaration. The first parameter is a function that takes 
+-- two things and produces a third thing. They don't have to be of the same type,
+-- but they can. The second and third parameter are lists. The result is also a
+-- list. The first has to be a list of a's, because the joining function takes
+-- a's as its first argument. The second has to be a list of b's, because the
+-- second parameter of the joining function is of type b. The result is a list
+-- of c's. If the type declaration of a function says it accepts an a -> b -> c
+-- function as a parameter, it will also accept an a -> a -> a function, but not
+-- the other way around! Remember that when you're making functions, especially
+-- higher order ones, and you're unsure of the type, you can just try omitting
+-- the type declaration and then checking what Haskell infers it to be by
+-- using :t.
