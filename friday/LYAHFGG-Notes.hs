fa82ef51bd66664affcd43ea4b8929a4b4285415
Patrick Allen <allenpatrick86@gmail.com>
2016-09-30 11:30:24 -0700

    add notes section for Friday

---
+++ friday/LYAHFGG-Notes.hs
@@ -0,0 +1,10 @@
+---------------------------- RECURSION ---------------------------------
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+-- Note: Num is not a subclass of Ord. That means that what constitutes for a 
+-- number doesn't really have to adhere to an ordering. So that's why we have to
+-- specify both the Num and Ord class constraints when doing addition or
+-- subtraction and also comparison.
