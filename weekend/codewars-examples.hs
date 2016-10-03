81061c97a531f5b99789f8f225b5d45900944e88
Patrick Allen <allenpatrick86@gmail.com>
2016-10-03 11:55:20 -0700

    add codewars file

---
+++ weekend/codewars-examples.hs
@@ -0,0 +1,12 @@
+-- Write function toInitials returs initials for a given person name.
+-- E.g: "Bill Gates" -> "B. G."
+
+module  Initials where
+
+toInitials :: String -> String
+
+toInitials [] = []
+toInitials xs = [firstInitial] ++ ". " ++ [secondInitial] ++ ['.']
+                where brokenName = words xs
+                      firstInitial = head (brokenName !! 0)
+                      secondInitial = head (brokenName !! 1)
