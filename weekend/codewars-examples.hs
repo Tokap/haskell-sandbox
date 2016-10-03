f639ae2954edba1390cec78805e26e17e2f5d7ae
Patrick Allen <allenpatrick86@gmail.com>
2016-10-03 11:56:07 -0700

    add codewars explanations

--- weekend/codewars-examples.hs
+++ weekend/codewars-examples.hs
@@ -1,3 +1,4 @@
+------------------------- SAMPLE QUESTION 1 ------------------------------------
 -- Write function toInitials returs initials for a given person name.
 -- E.g: "Bill Gates" -> "B. G."
 
